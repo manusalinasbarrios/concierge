6b1625058c25729b3ea3418097c26c947872a294
manuelsalinas69 <manuelsalinas69@gmail.com>
2026-04-29 19:55:43 -0300

    Ajustes para apuntar a cloud usando yaml

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
-       new URL('https://delightful-crown-636634ff69.media.strapiapp.com/**'),
       {
         protocol: 'http',
         hostname: 'localhost',
