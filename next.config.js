4881277899b7afaab07a17bbc87d0f51d9644212
manuelsalinas69 <manuelsalinas69@gmail.com>
2026-04-29 20:44:14 -0300

    next config para permitir imagenes de strapi

--- next.config.js
+++ next.config.js
@@ -3,13 +3,14 @@ const nextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [
+      new URL('https://delightful-crown-636634ff69.media.strapiapp.com/**'),
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '1337',
         pathname: '/uploads/**',
       },
-       {
+      {
         protocol: 'https',
         hostname: 'delightful-crown-636634ff69.media.strapiapp.com',
         port: '443',
